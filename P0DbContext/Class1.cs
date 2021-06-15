3c3a16bfa5920de52b3bb582e0712995b91475be
ChristianRevature <christian.romero@revature.net>
2021-06-15 08:54:43 -0400

    polishes P0 and presentation

--- P0DbContext/Class1.cs
+++ P0DbContext/Class1.cs
@@ -4,7 +4,5 @@ namespace P0DbContext
 {
     public class Class1
     {
-        
-         
     }
 }
---
+++ P0DbContext/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace P0DbContext
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public int CustomerId { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public int? Age { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool? Member { get; set; }
+        public string Email { get; set; }
+        public string Mailing { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int? Zip { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
