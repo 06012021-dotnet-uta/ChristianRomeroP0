2b13f4c9b560e41de2cc71ff3765b15245c8ed34
ChristianRevature <christian.romero@revature.net>
2021-07-02 10:22:52 -0400

    added notes

--- hr/wk4/Program.cs
+++ hr/wk4/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace wk4
+namespace HackerFour
 {
-    class Program
+     class Program
     {
-        static void Main(string[] args)
+        Stair stair = new Stair();
+         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //create a staircase of size N
+            /*Reqs:
+             * 1 - takes in a parameter of integer N [0,100]
+             * 2 - prints a staircase of # symbol
+             * 3- 
+            */
+
+
+
+            //Messages.ObtainConsent();
+
+            //Stair.PrintStaircase(number)
+
+            stair.GetInput();
+            stair.PrintStaircase(stair.number);
+                
         }
-    }
-}
+    }//eoc
+}//eon
